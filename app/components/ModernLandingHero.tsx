c080beb47f26b7da2d920da511acf93089c159f2
dhimiterPoison <dhimiter.helmi@gmail.com>
2024-03-20 02:18:15 +0100

    short text and immediately after the animation to catch attention and reinforce the concept

--- app/components/ModernLandingHero.tsx
+++ app/components/ModernLandingHero.tsx
@@ -24,19 +24,23 @@ export default function ModernLandingHero() {
 					</span>
 					<br />
 					<br />
-					<span className='text-xl'>
-						Make <em>peace</em> with ADHD and take advantage of your
-						creativity. <br />
-						<br />
-						Build systems <strong>tailored</strong> on Your Self,{' '}
-						<br />
-						store your thoughts, ideas, and feelings in a{' '}
-						<strong>safe</strong> place <br />
-						and let Self help you organizing your time so you can
-						use your energies more consciusly.
-					</span>
+					<div className=''>
+						<span className='text-xl'>Make <em>peace</em> with ADHD and take advantage of your
+						creativity. <br /></span>
+						{/* <br /> */}
+						<span className='text-xl'>Build systems <strong>tailored</strong> on Your Self.</span>
+						<div className='svganimation-systems  h-40 lg:h-48 w-full py-4'>
+							<div className='h-full w-full bg-emerald-600/50'></div>
+						</div>
+						<span className='text-lg'>
+							Store your thoughts, ideas, and feelings in a{' '}
+							<strong>safe</strong> place <br />
+							and let Self help you organizing your time so you
+							can use your energies more consciusly.
+						</span>
+					</div>
 				</div>
-				<div className='CTA flex w-full flex-col-reverse items-center justify-center gap-4 py-4 lg:py-8'>
+				<div className='CTA flex w-full flex-row md:flex-col-reverse items-center justify-center  gap-12 md:gap-8 py-4 lg:py-8'>
 					<div className='flex flex-col items-center'>
 						<Button variant='link' className=' text-base underline'>
 							Tell me more
@@ -49,15 +53,15 @@ export default function ModernLandingHero() {
 
 					<Link
 						href='/dashboard'
-						className='flex w-full items-center justify-center gap-4 '
+						className='flex md:w-full items-center justify-center gap-4 '
 					>
 						<Input
-							className='hidden h-full rounded-full shadow-sm shadow-green-900 border border-slate-200  px-8 text-base md:block'
+							className='hidden h-full rounded-full border border-slate-200 px-8 text-base  shadow-sm shadow-green-900 md:block'
 							placeholder='Type your email'
 						></Input>
 						<ButtonMovingBorder
 							variant='outline'
-							className='rounded-full border-green-900 border-none shadow-md shadow-green-600 bg-green-100 text-base font-bold text-primary duration-200 ease-in-out active:scale-110'
+							className='rounded-full border-none border-green-900 bg-green-100 text-base font-bold text-primary shadow-md shadow-green-600 duration-200 ease-in-out active:scale-110'
 						>
 							Join waitlist
 							<ArrowRightIcon
